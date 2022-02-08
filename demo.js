9b8f137160eec08cf2067c50d960bbecdcadefa9
Adam Franco <afranco@middlebury.edu>
2022-02-07 23:22:23 -0500

    Move style-specific row-height callback to our demo from OpenMapSamples.
    This is really style specific and shouldn't be in the sample itself.

--- demo.js
+++ demo.js
@@ -4,7 +4,7 @@ import SampleControl from './OpenMapSamplesControl';
 import Sample from "openmapsamples/lib/Sample";
 import Layer from "openmapsamples/lib/Layer";
 import { ExamplePlaceData, ExampleTransportationData } from "openmapsamples/lib/SampleData/ExampleSampleData";
-import highwayAttributesSample from "openmapsamples/samples/OpenMapTiles/HighwayAttributes";
+import { default as highwayAttributesSample, sampleData as highwayAttributesSampleData } from "openmapsamples/samples/OpenMapTiles/HighwayAttributes";
 // import MaplibreInspect from "maplibre-gl-inspect";
 // import "maplibre-gl-inspect/dist/maplibre-gl-inspect.css";
 
@@ -28,7 +28,20 @@ async function main() {
   // Add our sample data.
   let sampleControl = new SampleControl();
 
+  // Add our Highway Attributes sample.
   sampleControl.addSample(highwayAttributesSample);
+  // Set a custom row-hight callback to give more space to motorways at high zooms.
+  // This isn't needed for all styles, but the osm-standard style
+  // inflates widths of motorways and trunk at high zooms.
+  highwayAttributesSampleData.setRowHeightCallback(function(zoom, row, numRows) {
+    if (zoom > 18 && row < 1) {
+      return 24 / numRows / Math.pow(2, zoom - 4);
+    } else if (zoom > 16 && row < 1) {
+      return 22 / numRows / Math.pow(2, zoom - 4);
+    } else {
+      return 20 / numRows / Math.pow(2, zoom - 4);
+    }
+  });
 
   let sample = sampleControl.addSample(
     new Sample('example', 'Example', 'This is an example sample showing hand-crafted GeoJSON that mocks OpenMapTiles data. (<a href="https://github.com/adamfranco/OpenMapSamples/blob/main/lib/SampleData/ExampleSampleData.js">source</a>)', [-100.85664, 39.71282], 7)
