64bed5b4026b0d3665ba8cea700d2c45ac1516c6
Adam Franco <afranco@middlebury.edu>
2022-02-07 23:57:17 -0500

    Use index for all OpenMapTiles samples.

--- demo.js
+++ demo.js
@@ -4,8 +4,7 @@ import SampleControl from './OpenMapSamplesControl';
 import Sample from "openmapsamples/lib/Sample";
 import Layer from "openmapsamples/lib/Layer";
 import { ExamplePlaceData, ExampleTransportationData } from "openmapsamples/lib/SampleData/ExampleSampleData";
-import { default as highwayAttributesSample, sampleData as highwayAttributesSampleData } from "openmapsamples/samples/OpenMapTiles/HighwayAttributes";
-import { default as highwayLinkAttributesSample, sampleData as highwayLinkAttributesSampleData } from "openmapsamples/samples/OpenMapTiles/HighwayLinkAttributes";
+import { default as OpenMapTilesSamples } from "openmapsamples/samples/OpenMapTiles";
 // import MaplibreInspect from "maplibre-gl-inspect";
 // import "maplibre-gl-inspect/dist/maplibre-gl-inspect.css";
 
@@ -29,34 +28,8 @@ async function main() {
   // Add our sample data.
   let sampleControl = new SampleControl();
 
-  // Add our Highway Attributes sample.
-  sampleControl.addSample(highwayAttributesSample);
-  // Set a custom row-hight callback to give more space to motorways at high zooms.
-  // This isn't needed for all styles, but the osm-standard style
-  // inflates widths of motorways and trunk at high zooms.
-  highwayAttributesSampleData.setRowHeightCallback(function(zoom, row, numRows) {
-    if (zoom > 18 && row < 1) {
-      return 24 / numRows / Math.pow(2, zoom - 4);
-    } else if (zoom > 16 && row < 1) {
-      return 22 / numRows / Math.pow(2, zoom - 4);
-    } else {
-      return 20 / numRows / Math.pow(2, zoom - 4);
-    }
-  });
-
-  // Add our Highway Link Attributes sample.
-  sampleControl.addSample(highwayLinkAttributesSample);
-  // Set a custom row-hight callback to give more space to motorways at high zooms.
-  // This isn't needed for all styles, but the osm-standard style
-  // inflates widths of motorways and trunk at high zooms.
-  highwayLinkAttributesSampleData.setRowHeightCallback(function(zoom, row, numRows) {
-    if (zoom > 18 && row < 2) {
-      return 24 / numRows / Math.pow(2, zoom - 4);
-    } else if (zoom > 16 && row < 2) {
-      return 22 / numRows / Math.pow(2, zoom - 4);
-    } else {
-      return 20 / numRows / Math.pow(2, zoom - 4);
-    }
+  OpenMapTilesSamples.forEach((sample, i) => {
+    sampleControl.addSample(sample);
   });
 
   let sample = sampleControl.addSample(
